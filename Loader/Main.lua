9aebb39fee2098c5032a66034f4cc2646723180e
NoLag-ldss <danielhartono99111@gmail.com>
2026-06-15 13:05:47 +0700

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastefy.app/Y8p1sPFJ/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AmhadV99/Speed-Hub-X/refs/heads/main/Speed%20Hub%20X", true))()
